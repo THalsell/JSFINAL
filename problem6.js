9fb64df6871b002120dafe441f9b412ea9dd3e92
THalsell <139520247+THalsell@users.noreply.github.com>
2023-11-30 18:48:55 -0600

    sixth commit

--- problem6.js
+++ problem6.js
@@ -1,14 +1,14 @@
 function getPlace(highScores, newScore) {
     if (newScore > highScores[0]) {
-        return "First Place"
+        return "Congratulations! You achieved first Place"
     } if (newScore < highScores[highScores.length -1]) {
-        return "Sorry you didn't make the score list."
+        return "Sorry you didn't make the high score list."
     }
     for (let i = 0; i < highScores.length; i++) {
         if (newScore >= highScores[i]) {
-            return "Congratulations"
+            return "Congratulations! You achieved 2nd place!"
         }
     }
 }
 
-console.log(getPlace([60, 30, 10], 90))
+console.log(getPlace([500, 300, 200], 100))
--- problem7.js
+++ problem7.js
@@ -1,1 +1,19 @@
-function addToObj()
+function addToObj(person, key, value) {
+    if (typeof key !== "string") {
+        return "Function must be called with a valid key."
+    }
+
+    person[key] = value;
+}
+
+
+
+let personObject = {
+    name: "tiffany",
+    birthday: "October 22",
+    age: 30
+}
+
+personObject = addToObj(personObject, "homestate", "tennessee")
+
+
